0ddb2a4dfce2c073a282b37779d442268d5c7dba
Renato66 <revi66@hotmail.com>
2019-03-24 18:31:03 -0300

    index layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,47 @@
 <template>
   <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <v-container fluid pa-0 grid-list-md>
+          <v-layout>
+            <v-flex xs4>
+              <img src="/img-1.jpg" alt="" style="max-width:100%">
+              <v-icon small color="primary" style="position:absolute">subdirectory_arrow_right</v-icon>
+              <p class="pl-3 grey--text text--darken-3 caption">
+                Entendemos que cada cliente é unico e possuimos um atendimento personalizado para cada tipo de negócio
+              </p>
+            </v-flex>
+            <v-flex xs4>
+              <img src="/img-2.jpg" alt="" style="max-width:100%">
+              <v-icon small color="primary" style="position:absolute">subdirectory_arrow_right</v-icon>
+              <p class="pl-3 grey--text text--darken-3 caption">
+                Flexibilidade na relação comercial desde da proposta de negócios à atualizações de contrato
+              </p>
+            </v-flex>
+            <v-flex xs4>
+              <img src="/img-3.jpg" alt="" style="max-width:100%">
+              <v-icon small color="primary" style="position:absolute">subdirectory_arrow_right</v-icon>
+              <p class="pl-3 grey--text text--darken-3 caption">
+                Fornecemos os recrusos qu você precisa para apoiar o seu sucesso
+              </p>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
     <v-layout mb-5>
       <v-flex xs7 pr-3>
         <h3 class="display-1 mb-3">
           Soluções em infraestrutura,<br>
           isso é <span class="primary--text font-weight-bold">Building infra</span>
         </h3>
-        <p class="grey--text darken-1">
+        <p class="grey--text text--darken-1">
           A segurança de rede não é mais um luxo para as grandes corporações e sim uma necessidade.
         </p>
-        <p  class="grey--text darken-1">
+        <p  class="grey--text text--darken-1">
           Nossa abordagem proativa e vigilante garante que a infraestrutura de sua empresa estea atualizada e protegida contra ameaças.
         </p>
-        <p  class="grey--text darken-1">
+        <p  class="grey--text text--darken-1">
           Vamos além de suporte ao usuário e manutenção computadores também nos especializamos ao proeto de implementação de estrutura de dados e sstemas, auxiliando a manter desde do projeto inicial até a segurança da internet.
         </p>
         <v-btn color="primary" class="ml-0" round outline>
@@ -29,10 +58,10 @@
         <h3 class="display-1 mb-3">
           Somos uma empresa<br><span class="primary--text font-weight-bold">totalmente diferente</span>
         </h3>
-        <p class="grey--text darken-1">
+        <p class="grey--text text--darken-1">
           Nós nos esforcamos para ser um perceiro que você pode confiar, independente do problema nos colocamos a disposição para ajudar a melhorar cada vez mais a funcionalidade da sua empresa.
         </p>
-        <p  class="grey--text darken-1">
+        <p  class="grey--text text--darken-1">
           Nos orgulhamos de não ser apenas o cada dos computadores e sim mais um membro da equipe, confiável e experiente que reflete sua paixão pelo seu negócioNos orgulhamos de não ser apenas o cada dos computadores e sim mais um membro da equipe, confiável e experiente que reflete sua paixão pelo seu negócio.
         </p>
         <v-btn color="primary" class="ml-0" round outline>
@@ -52,7 +81,7 @@
                 Confira<br>
                 <span class="primary--text font-weight-bold">nossos serviços</span>
               </h5>
-              <p>
+              <p class="grey--text text--darken-3">
                 entendemos que clientes possuem nescessidades únicas e
                 é desta forma que cada cliente é tratado. Para tanto 
                 procuramos entender junto com cada cliente suas necessidades
