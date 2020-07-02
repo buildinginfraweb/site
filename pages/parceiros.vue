bce4ef5436d0145693d99a6a305113deef15d734
Renato Vicente <revi66@hotmail.com>
2020-07-02 01:35:42 -0300

    page parceiros created

---
+++ pages/parceiros.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <CommentsCarousel />
+  </v-container>
+</template>
+
+<script>
+import CommentsCarousel from '~/components/CommentsCarousel'
+export default {
+  components: {
+    CommentsCarousel
+  }
+}
+</script>
+
+<style>
+
+</style>
