c355cf73362840c87181cb02fc93786c88c7a719
renato.santos <renato.santos@doc88.com.br>
2020-12-28 12:09:29 -0300

    chore: remove under maintence

---
+++ pages/parceiros.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12 class="white--text text-xs-center" data-aos="fade-left">
+        <h2 class="display-1 mb-5">
+          O brilho da
+          <span class="white px-2 primary--text font-weight-bold"
+            >Building infra</span
+          ><br />
+          não seria nada sem eles
+        </h2>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 data-aos="fade-up">
+        <CommentsCarousel />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import CommentsCarousel from '~/components/CommentsCarousel'
+export default {
+  components: {
+    CommentsCarousel
+  }
+}
+</script>
+
+<style></style>
