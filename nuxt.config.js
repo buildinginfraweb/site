b51224d91644b5c978d01108fdb81ff4b3be317c
renato.santos <renato.santos@doc88.com.br>
2021-02-02 01:46:22 -0300

    fix: better title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'Building Infra',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
